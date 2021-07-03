009651b1d4536518a333efcd9deafc1ac5795ae8
NIKHIL <63747907+nikhiljha261@users.noreply.github.com>
2021-07-03 19:32:09 +0530

    Create printdivisor.cpp

---
+++ printdivisor.cpp
@@ -0,0 +1,15 @@
+void printDivisors(int n)
+{
+    for (int i=1; i<=sqrt(n); i++)
+    {
+        if (n%i == 0)
+        {
+            // If divisors are equal, print only one
+            if (n/i == i)
+                printf("%d ", i);
+ 
+            else // Otherwise print both
+                printf("%d %d ", i, n/i);
+        }
+    }
+}
